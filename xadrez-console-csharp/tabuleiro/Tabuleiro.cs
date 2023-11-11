0eeccfbcf6b08f146eb1da8c0829dd18e7ad046e
Leandro Junior Alves Dos Santos <ljelectrar@gmail.com>
2023-11-11 15:24:52 -0300

    Adicionado tratamento de erros personalizada

--- xadrez-console-csharp/tabuleiro/Tabuleiro.cs
+++ xadrez-console-csharp/tabuleiro/Tabuleiro.cs
@@ -19,10 +19,45 @@
             return pecas[linha, coluna];
         }
 
+        public Peca Peca (Posicao pos)
+        {
+            return pecas[pos.Linha, pos.Coluna];
+        }
+
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return Peca(pos) != null;
+        }
+
         public void colocarPeca(Peca p, Posicao pos)
         {
+            if(existePeca(pos))
+            {
+                throw new TabuleiroException("Já existe uma peça nesta posição");
+            }
             pecas[pos.Linha, pos.Coluna] = p;
             p.Posicao = pos;
+        }
+
+        public bool posicaoValida(Posicao pos)
+        {
+            if (pos.Linha < 0 || pos.Coluna>= Linhas || pos.Coluna>=  Colunas)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void validarPosicao(Posicao pos)
+        {
+            if(!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição Inválida");
+            }
         }
     }
 }
