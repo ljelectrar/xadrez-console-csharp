097a48a391937fd7acc2abef9e54322dfb8419d4
Leandro Junior Alves Dos Santos <ljelectrar@gmail.com>
2023-11-11 12:59:31 -0300

    Classes adicionadas: Cor, Peca, Tabuleiro.

---
+++ xadrez-console-csharp/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,17 @@
+﻿namespace tabuleiro
+{
+    class Tabuleiro
+    {
+        public int Linhas { get; init; }
+        public int Colunas { get; set; }
+
+        private Peca[,] pecas;
+
+        public Tabuleiro(int linhas, int colunas)
+        {
+            Linhas = linhas;
+            Colunas = colunas;
+            pecas = new Peca[Linhas, Colunas];
+        }
+    }
+}
